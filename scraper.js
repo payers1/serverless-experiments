d5efe41140ed0aa90386becdbb80a040f25a30b3
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-06-01 21:26:38 -0400

    Refactor

--- scraper.js
+++ scraper.js
@@ -1,41 +1,62 @@
 const x   = require('x-ray')();
 const fs  = require('fs');
 const _   = require('lodash');
+const P   = require('bluebird');
 
 const url = 'http://economistsquares.economist.com/Game/index.php';
 const selector = 'script:nth-child(10)';
 
-x(url, selector) ((err, script) => {
-  if (!err) {
-    const lines = script
-      .split('\n')
-      .map(line => line.replace(/(\t|\\)/g, ''))
-      .filter(line => line.startsWith('qvals'))
-      .reduce((prev, line) => {
-        const qNum = line.substr(6, 1);
-        if (!prev[qNum]) {
-          prev[qNum] = {
-            options: []
-          };
+function scrapeForQuestions() {
+  x(url, selector) ((err, script) => {
+    if (!err) {
+      const lines = script
+        .split('\n')
+        .map(line => line.replace(/(\t|\\)/g, ''))
+        .filter(line => line.startsWith('qvals'))
+        .reduce((prev, line) => {
+          const qNum = line.substr(6, 1);
+          if (!prev[qNum]) {
+            prev[qNum] = {
+              options: []
+            };
+          }
+          q = prev[qNum];
+
+        const equalSign = line.indexOf('=') + 4;
+        const len = line.length - 3;
+        const value = line.substring(equalSign, len).trim();
+
+        if (line.includes('["question"]')) {
+          q.questionText = value;
+        } else if (line.includes('questionid')) {
+          q.id = value;
+        } else if (line.includes('["option')) {
+          q.options = [...q.options, value];
         }
-        q = prev[qNum];
+        return prev;
+      }, {});
 
-      const equalSign = line.indexOf('=') + 4;
-      const len = line.length - 3;
-      const value = line.substring(equalSign, len).trim();
+      const obj = _.mapKeys(lines, ({id}) => id);
 
-      if (line.includes('["question"]')) {
-        q.questionText = value;
-      } else if (line.includes('questionid')) {
-        q.id = value;
-      } else if (line.includes('["option')) {
-        q.options = [...q.options, value];
-      }
-      return prev;
-    }, {});
+      fs.writeFileSync(__dirname + '/questions.js', JSON.stringify(obj, null, 2));
+    }
+  });
+}
 
-    const obj = _.mapKeys(lines, ({id}) => id);
+async function scrapeForQuestionsDate() {
+  const data = await P.fromCallback(x(url, selector))
+  const line = data.split('\n')
+             .find(l => l.includes('questiondates'))
+             .trim();
 
-    fs.writeFileSync(__dirname + '/questions.js', JSON.stringify(obj, null, 2));
-  }
-});
+  const equalSign = line.indexOf('=') + 17;
+  const len = line.length - 2;
+  return line.substring(equalSign, len).trim();
+}
+
+module.exports = {
+  scrapeForQuestions,
+  scrapeForQuestionsDate
+}
+
+require('make-runnable');
