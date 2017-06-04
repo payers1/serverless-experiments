4713a2f6c63661025e58b548c28fdda835d44d56
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-06-04 10:04:50 -0400

    Add endpoint to get a random question

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const app  = require('express')();
 const PORT = process.env.PORT || 3000;
+const getRandomQuestion = () => {
+  const questions = require('./questions.json');
+  const questionKeys = _.keys(questions);
+  const randomNum = _.random(0, questionKeys.length - 1);
+  const randomQuestionKey = questionKeys[randomNum];
+  return questions[randomQuestionKey];
+}
 
 app.get('/', (req, res) => {
   res.send('it works');
 })
 
+app.get('/random-question', (req, res) => {
+  res.send(getRandomQuestion());
+})
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
