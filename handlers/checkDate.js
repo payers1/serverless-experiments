80dc90424c175c9ec2ed3f62d6c95a8246eeac6b
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-06-12 20:00:24 -0400

    feat: Scrape for new questions based on days since last question set

--- handlers/checkDate.js
+++ handlers/checkDate.js
@@ -1,8 +1,14 @@
 const scraper = require('../scraper');
+const moment = require('moment');
 
 module.exports = (req, res) => {
   return scraper.scrapeForQuestionsDate()
     .then((a) => {
+      const d = moment(a, "MMMM DD YYYY");
+      const diff = moment().diff(d, 'days');
+      if (diff > 5) {
+        scraper.scrapeForQuestions();
+      }
       res.send(a);
     });
 }
