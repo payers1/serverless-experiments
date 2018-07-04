b4b91ee6889319036df26a0d1a530860470c7889
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-07-03 21:54:51 -0400

    refactor

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const serverless  = require('serverless-http');
-const bodyParser  = require('body-parser');
-const app         = require('express')();
-const questions   = require('./handlers/questions')
+const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
+const app = require('express')()
+const questions = require('./handlers/questions')
 
-app.use(bodyParser.json({ strict: false }));
+app.use(bodyParser.json({ strict: false }))
 app.get('/', (req, res) => res.send('it works'))
+app.get('/questions', questions.getAllQuestions)
 app.get('/questions/:questionId', questions.getQuestion)
-app.post('/questions', questions.saveQuestion)
+app.post('/questions', questions.saveAllQuestions)
 
 module.exports.handler = serverless(app)
