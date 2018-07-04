07e609682a13cddef632ac285bf82d8c77daa8d0
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-07-03 21:53:41 -0400

    refactor caper to use promises

--- scraper.js
+++ scraper.js
@@ -6,45 +6,37 @@ const P = require('bluebird')
 const url = 'http://economistsquares.economist.com/Game/index.php'
 const selector = 'script:nth-child(10)'
 
-function scrapeForQuestions() {
-  x(url, selector)((err, script) => {
-    if (!err) {
-      const lines = script
-        .split('\n')
-        .map(line => line.replace(/(\t|\\)/g, ''))
-        .filter(line => line.startsWith('qvals'))
-        .reduce((prev, line) => {
-          const qNum = line.substr(6, 1)
-          if (!prev[qNum]) {
-            prev[qNum] = {
-              options: []
-            }
-          }
-          q = prev[qNum]
+async function scrapeForQuestions() {
+  const data = await P.fromCallback(x(url, selector))
+  const lines = data
+    .split('\n')
+    .map(line => line.replace(/(\t|\\)/g, ''))
+    .filter(line => line.startsWith('qvals'))
+    .reduce((prev, line) => {
+      const qNum = line.substr(6, 1)
+      if (!prev[qNum]) {
+        prev[qNum] = {
+          options: []
+        }
+      }
+      q = prev[qNum]
 
-          const equalSign = line.indexOf('=') + 4
-          const len = line.length - 3
-          const value = line.substring(equalSign, len).trim()
+      const equalSign = line.indexOf('=') + 4
+      const len = line.length - 3
+      const value = line.substring(equalSign, len).trim()
 
-          if (line.includes('["question"]')) {
-            q.questionText = value
-          } else if (line.includes('questionid')) {
-            q.id = value
-          } else if (line.includes('["option')) {
-            q.options = [...q.options, value]
-          }
-          return prev
-        }, {})
+      if (line.includes('["question"]')) {
+        q.questionText = value
+      } else if (line.includes('questionid')) {
+        q.id = value
+      } else if (line.includes('["option')) {
+        q.options = [...q.options, value]
+      }
+      return prev
+    }, {})
 
-      const obj = _.mapKeys(lines, ({ id }) => id)
-      console.log(obj)
-
-      fs.writeFileSync(
-        __dirname + '/questions.json',
-        JSON.stringify(obj, null, 2)
-      )
-    }
-  })
+  const obj = _.mapKeys(lines, ({ id }) => id)
+  return obj
 }
 
 async function scrapeForQuestionsDate() {
@@ -63,5 +55,3 @@ module.exports = {
   scrapeForQuestions,
   scrapeForQuestionsDate
 }
-
-require('make-runnable')
