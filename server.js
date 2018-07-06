f50a75778791d4f850289bba636cfdd0f3acc4c8
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-07-06 19:43:05 -0400

    delete scraper
    Economist no longer publishes news quizzes

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const serverless = require('serverless-http')
 const bodyParser = require('body-parser')
 const app = require('express')()
-const questions = require('./handlers/questions')
 
 app.use(bodyParser.json({ strict: false }))
 app.get('/', (req, res) => res.send('it works'))
-app.get('/questions', questions.getAllQuestions)
-app.get('/questions/:questionId', questions.getQuestion)
-app.post('/questions', questions.saveAllQuestions)
 
 module.exports.handler = serverless(app)
