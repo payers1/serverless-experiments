e1788373196c1bf172575281e0cc403f47504c86
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-07-22 20:59:49 -0400

    dynamo db helper

---
+++ db.js
@@ -0,0 +1,11 @@
+const AWS = require('aws-sdk')
+const { IS_OFFLINE } = process.env
+const dbConfig =
+  IS_OFFLINE === 'true'
+    ? {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+      }
+    : undefined
+
+module.exports = new AWS.DynamoDB.DocumentClient()
